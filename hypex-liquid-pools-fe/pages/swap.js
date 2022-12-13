e5368e8409e619b3d7abf2508f4382115facaed2
kittyapes <109080669+kittyapes@users.noreply.github.com>
2022-12-12 23:54:22 -0800

    detail page(intro + tabs)

--- hypex-liquid-pools-fe/pages/swap.js
+++ hypex-liquid-pools-fe/pages/swap.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Landing from "../components/home/Landing";
+import Details from "../components/pool/details/Details";
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+          <Details />
       </main>
     </div>
   )
